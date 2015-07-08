7f7cf80d65256365ffc15e15195ef65a6e149615
Niklas <niklas@niklasandersson.net>
2015-07-08 10:35:52 +0200

    Define any vector using 3-component array

--- js/modules/graphics/factories/Axis.js
+++ js/modules/graphics/factories/Axis.js
@@ -1,13 +1,12 @@
 graphics.factory('Axis', ['Vector', function(Vector) {
   return {
     create: function(size) {
+      console.log('axis');
       var axis = new THREE.Mesh();
-      var xAxis = Vector.create(0.03, size, 0xff0000);
-      xAxis.rotation.z = -Math.PI/2.0;
-      var yAxis = Vector.create(0.03, size, 0x00ff00);
-      yAxis.rotation.x = -Math.PI/2.0;
-      var zAxis = Vector.create(0.03, size, 0x0000ff);
-
+      var xAxis = Vector.create([1, 0, 0], 0.03, size, 0xff0000);
+      var yAxis = Vector.create([0, 1, 0], 0.03, size, 0x00ff00);
+      var zAxis = Vector.create([0, 0, 1], 0.03, size, 0x0000ff);
+      console.log('axis');
       axis.add(xAxis);
       axis.add(yAxis);
       axis.add(zAxis);
--- js/modules/graphics/factories/Vector.js
+++ js/modules/graphics/factories/Vector.js
@@ -1,18 +1,40 @@
 graphics.factory('Vector', function() {
-  return {
-    create: function(width, height, color) {
-      var bodyGeometry = new THREE.CylinderGeometry(width, width, height, 50, 50, false);
-      var bodyMaterial = new THREE.MeshPhongMaterial( { color: color} );
-      var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
 
-      var headGeometry = new THREE.CylinderGeometry(0, 2*width, 0.1, 50, 50, false);
-      var headMaterial = new THREE.MeshPhongMaterial( { color: color } );
-      var head = new THREE.Mesh(headGeometry, headMaterial);
+  function create(vector, width, height, color) {
+    var defaultVector = new THREE.Vector3(0,1,0);
 
-      head.position.y = height/2;
-      body.add(head);
+    var vectorAngle = defaultVector.angleTo(vector);
+    var vectorAxis = new THREE.Vector3();
+    vectorAxis.crossVectors(defaultVector, vector);
+    vectorAxis.normalize();
 
-      return body;
-    }
+    var bodyGeometry = new THREE.CylinderGeometry(width, width, height, 50, 50, false);
+    var bodyMaterial = new THREE.MeshPhongMaterial( { color: color} );
+    var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+
+    var headGeometry = new THREE.CylinderGeometry(0, 2*width, 0.1, 50, 50, false);
+    var headMaterial = new THREE.MeshPhongMaterial( { color: color } );
+    var head = new THREE.Mesh(headGeometry, headMaterial);
+
+    head.position.y = height/2;
+    body.add(head);
+
+    body.rotateOnAxis(vectorAxis, vectorAngle);
+
+    return body;
+  }
+
+  var Vector = function(spec) {
+    this.vector = new THREE.Vector3();
+    this.vector.fromArray(spec.vector || [1, 1, 1]);
+    this.color = spec.color || 0xff0000;
+    this.width = spec.width || 0.01;
+    this.length = spec.length || 1;
+    this.object = create(this.vector, this.width, this.length, this.color);
+
+    this.goalVector = new THREE.Vector3(0,0,0);
+    this.animationSpeed = 0;
   };
+
+  return Vector;
 });
