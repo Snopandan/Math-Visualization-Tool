183242ded1b47bb8122b2090df3efb3761d7ac6a
Niklas <niklas@niklasandersson.net>
2015-07-08 10:36:29 +0200

    Use the new vector interface

--- js/modules/graphics/factories/Axis.js
+++ js/modules/graphics/factories/Axis.js
@@ -1,15 +1,20 @@
 graphics.factory('Axis', ['Vector', function(Vector) {
   return {
     create: function(size) {
-      console.log('axis');
       var axis = new THREE.Mesh();
-      var xAxis = Vector.create([1, 0, 0], 0.03, size, 0xff0000);
-      var yAxis = Vector.create([0, 1, 0], 0.03, size, 0x00ff00);
-      var zAxis = Vector.create([0, 0, 1], 0.03, size, 0x0000ff);
-      console.log('axis');
-      axis.add(xAxis);
-      axis.add(yAxis);
-      axis.add(zAxis);
+      var xAxis = new Vector({vector: [1, 0, 0], width: 0.03, length: size, color: 0xff0000});
+      var yAxis = new Vector({vector: [0, 1, 0], color: 0x00ff00});
+      var zAxis = new Vector({vector: [0, 0, 1], width: 0.03, length: size, color: 0x0000ff});
+      axis.add(xAxis.object);
+      axis.add(yAxis.object);
+      axis.add(zAxis.object);
+
+      xAxis.animate();
+      xAxis.update();
+      yAxis.animate();
+      yAxis.update();
+      zAxis.animate();
+      zAxis.update();
 
       return axis;
     }
