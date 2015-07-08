afdec614c2032f0b6d4a26351a69b3d9517ee13c
Niklas <niklas@niklasandersson.net>
2015-07-08 12:00:37 +0200

    Add animation
    The vector can now be animated by first calling setAnimationProperties
    and specify the goal vector and speed and then calling animate every frame.

--- js/modules/graphics/factories/Vector.js
+++ js/modules/graphics/factories/Vector.js
@@ -3,11 +3,6 @@ graphics.factory('Vector', function() {
   function create(vector, width, height, color) {
     var defaultVector = new THREE.Vector3(0,1,0);
 
-    var vectorAngle = defaultVector.angleTo(vector);
-    var vectorAxis = new THREE.Vector3();
-    vectorAxis.crossVectors(defaultVector, vector);
-    vectorAxis.normalize();
-
     var bodyGeometry = new THREE.CylinderGeometry(width, width, height, 50, 50, false);
     var bodyMaterial = new THREE.MeshPhongMaterial( { color: color} );
     var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
@@ -19,14 +14,26 @@ graphics.factory('Vector', function() {
     head.position.y = height/2;
     body.add(head);
 
-    body.rotateOnAxis(vectorAxis, vectorAngle);
+    var rotation = computeRotation(defaultVector, vector);
+    body.rotateOnAxis(rotation.axis, rotation.angle);
 
     return body;
+  }
+
+  function computeRotation(from, to) {
+    var vectorAngle = from.angleTo(to);
+    var vectorAxis = new THREE.Vector3();
+    vectorAxis.crossVectors(from, to);
+    vectorAxis.normalize();
+
+    return {angle: vectorAngle, axis: vectorAxis};
   }
 
   var Vector = function(spec) {
     this.vector = new THREE.Vector3();
     this.vector.fromArray(spec.vector || [1, 1, 1]);
+    this.currentVector = new THREE.Vector3();
+    this.currentVector.copy(this.vector);
     this.color = spec.color || 0xff0000;
     this.width = spec.width || 0.01;
     this.length = spec.length || 1;
@@ -34,6 +41,27 @@ graphics.factory('Vector', function() {
 
     this.goalVector = new THREE.Vector3(0,0,0);
     this.animationSpeed = 0;
+    this.animationStep = 0;
+  };
+
+  Vector.prototype.setAnimationProperties = function(params) {
+    this.goalVector.fromArray(params.vector || [1, 1, 1]);
+    this.animationSpeed = 'speed' in params ? params.speed : 0.01;
+  };
+
+  Vector.prototype.animate = function() {
+    if (this.animationSpeed === 0) {
+        return;
+    }
+
+    this.animationStep += this.animationSpeed;
+    this.animationStep = this.animationStep >= 1 ? 1 : this.animationStep;
+    var lerpVector = new THREE.Vector3();
+    lerpVector.lerpVectors(this.vector, this.goalVector, this.animationStep);
+
+    var rotation = computeRotation(this.currentVector, lerpVector);
+    this.currentVector.copy(lerpVector);
+    this.object.rotateOnAxis(rotation.axis, rotation.angle);
   };
 
   return Vector;
