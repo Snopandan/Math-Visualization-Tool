2f4cfb29782990621cdf21fa518360c26729b7c8
Niklas <niklas@niklasandersson.net>
2015-07-13 11:13:15 +0200

    Add color to y-axis

--- js/modules/graphics/factories/Axis.js
+++ js/modules/graphics/factories/Axis.js
@@ -3,7 +3,7 @@ graphics.factory('Axis', ['Vector', function(Vector) {
     create: function(size) {
       var axis = new THREE.Mesh();
       var xAxis = new Vector({vector: [1, 0, 0], width: 0.03, length: size, color: 0xff0000});
-      var yAxis = new Vector({vector: [0, 1, 0], color: 0x00ff00});
+      var yAxis = new Vector({vector: [0, 1, 0], width: 0.03, length: size, color: 0x00ff00});
       var zAxis = new Vector({vector: [0, 0, 1], width: 0.03, length: size, color: 0x0000ff});
       axis.add(xAxis.object);
       axis.add(yAxis.object);
