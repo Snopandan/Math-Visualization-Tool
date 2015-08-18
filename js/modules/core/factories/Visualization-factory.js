62fde883ce3b3bd0a77d11072ead8ba3cf86ade0
Niklas <niklas@niklasandersson.net>
2015-08-18 15:26:16 +0200

    Add function to clear scene

--- js/modules/core/factories/Visualization-factory.js
+++ js/modules/core/factories/Visualization-factory.js
@@ -3,6 +3,7 @@ core.factory('Visualization', ['Scene', function(Scene) {
   var Visualization = function() {
     var that = {};
 
+    var name = '';
     var scene = Scene();
     var stageDescriptions = [];
     var stageCallbacks = [];
@@ -26,12 +27,24 @@ core.factory('Visualization', ['Scene', function(Scene) {
       return stageCallbacks[index - 1];
     };
 
+    that.setName = function(n) {
+      name = n;
+    };
+
+    that.getName = function() {
+      return name;
+    };
+
     that.add = function(object) {
       scene.add(object);
     };
 
     that.getScene = function() {
       return scene;
+    };
+
+    that.clearScene = function() {
+      scene.clear();
     };
 
     return that;
