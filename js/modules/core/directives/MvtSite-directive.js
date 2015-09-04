a38b19bf29ef522a18e4f83fdb44f4c85f2ea5e6
Niklas Andersson <niklas@niklasandersson.net>
2015-09-04 15:58:20 +0200

    Add open and info dialogs

--- js/modules/core/directives/MvtSite-directive.js
+++ js/modules/core/directives/MvtSite-directive.js
@@ -7,11 +7,27 @@ CoreModule.directive('mvtSite', ['MultiTransclude', function(MultiTransclude) {
               '<div class="gui sidebar">'+
                 '<div id="content" transclude-id="gui-content"></div>'+
               '</div>' +
-              '<div title="Open a visualization" class="gui circle button open"><img class="buttonImage" src="images/icons/open.png" /></div>' +
+              '<div ng-click="toggleOpenOverlay()" title="Open a visualization" class="gui circle button open"><img class="buttonImage" src="images/icons/open.png" /></div>' +
               '<a ng-href="#/pattern/"><div ng-href="/pattern" title="Create a new visualization" class="gui circle button add"><img class="buttonImage" src="images/icons/add.png" /></div></a>' +
-              '<div title="Information about this application" class="gui circle button info"><img class="buttonImage" src="images/icons/info.png" /></div>',
+              '<div ng-click="toggleInfoOverlay()" title="Information about this application" class="gui circle button info"><img class="buttonImage" src="images/icons/info.png" /></div>' +
+              '<div ng-show="showInfoOverlay" class="overlay">Info</div>' +
+              '<div ng-show="showOpenOverlay" class="overlay">Open</div>',
     link: function(scope, element, attributes, controller, transclude) {
       MultiTransclude.transclude(element, transclude);
+      scope.showInfoOverlay = false;
+      scope.showOpenOverlay = false;
+      scope.toggleInfoOverlay = function() {
+        scope.showInfoOverlay = !scope.showInfoOverlay;
+        if (scope.showInfoOverlay) {
+          scope.showOpenOverlay = false;
+        }
+      };
+      scope.toggleOpenOverlay = function() {
+        scope.showOpenOverlay = !scope.showOpenOverlay;
+        if (scope.showOpenOverlay) {
+          scope.showInfoOverlay = false;
+        }
+      };
     }
   };
 }]);
