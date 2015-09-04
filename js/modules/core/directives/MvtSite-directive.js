74eeff57029a53d30fa3874868e8856ef6462271
Niklas Andersson <niklas@niklasandersson.net>
2015-09-04 15:20:33 +0200

    Add link to Pattern route

--- js/modules/core/directives/MvtSite-directive.js
+++ js/modules/core/directives/MvtSite-directive.js
@@ -8,7 +8,7 @@ CoreModule.directive('mvtSite', ['MultiTransclude', function(MultiTransclude) {
                 '<div id="content" transclude-id="gui-content"></div>'+
               '</div>' +
               '<div title="Open a visualization" class="gui circle button open"><img class="buttonImage" src="images/icons/open.png" /></div>' +
-              '<div title="Create a new visualization" class="gui circle button add"><img class="buttonImage" src="images/icons/add.png" /></div>' +
+              '<a ng-href="#/pattern/"><div ng-href="/pattern" title="Create a new visualization" class="gui circle button add"><img class="buttonImage" src="images/icons/add.png" /></div></a>' +
               '<div title="Information about this application" class="gui circle button info"><img class="buttonImage" src="images/icons/info.png" /></div>',
     link: function(scope, element, attributes, controller, transclude) {
       MultiTransclude.transclude(element, transclude);
